d84c91966a5b8be02c20b10b81372b3956490a6f
Juan Di Pasquo <juandipasquo@gmail.com>
2021-11-12 17:13:23 -0300

    Try Except

--- debugging.py
+++ debugging.py
@@ -1,16 +1,20 @@
 def divisors(num):
-    # List comprehension form
     divisors = [i for i in range(1, num + 1) if num %i == 0]
-    # Normal form
-    # for i in range(1 , num + 1):
-    #     if num %i == 0:
-    #         divisors.append(i)
+
     return divisors
 
 def run():
-    num = int(input('Ingrese un numero: '))
-    print(divisors(num))
-    print('* * * Termino mi programa * * *')
+    while True:
+        try:
+            num = int(input('Ingrese un numero: '))
+            if num < 0:
+                raise ValueError('\nPor favor ingrese un numero mayor a cero')
+            print(divisors(num))
+            print('* * * Termino mi programa * * *')
+            break
+        except ValueError as ve:
+            print(ve)
+            print('Debes ingresar un valor valido')
 
 
 if __name__ == '__main__':
